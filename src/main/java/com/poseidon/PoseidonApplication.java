4422ab714193d7782dd0f48b55f5e0e0dc216f33
Will <willahrons@hotmail.com>
2015-12-25 18:46:33 -0200

    First commit
    comitando o projeto gerado pelo spring initializer.

---
+++ src/main/java/com/poseidon/PoseidonApplication.java
@@ -0,0 +1,12 @@
+package com.poseidon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoseidonApplication.class, args);
+    }
+}
