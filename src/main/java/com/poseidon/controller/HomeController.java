c41df41f3fc61958273d04c71e843bb932504f98
Will <willahrons@hotmail.com>
2015-12-26 02:05:05 -0200

    adicionando os imports do @controller

--- src/main/java/com/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidon/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.poseidon.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
