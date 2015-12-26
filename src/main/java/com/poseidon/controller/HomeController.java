729995e65e8673e0c0a216196d2df158c4d10081
Will <willahrons@hotmail.com>
2015-12-26 02:04:05 -0200

    adicionando o @Controller no Home Controller e refatorando o stylesheet do pesqusiarPaciente

--- src/main/java/com/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidon/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.poseidon.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HomeController {
 	 @RequestMapping("/home")
 	    public ModelAndView home(ModelAndView  modelAndView){
