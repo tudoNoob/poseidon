80fd532345b9a9cbcd74903ed010d04b5e7153de
Pedro Silva <pedrro2@hotmail.com>
2015-12-26 00:25:05 -0200

    Crianda Pesquisa

--- src/main/java/com/poseidon/controller/PacienteController.java
+++ src/main/java/com/poseidon/controller/PacienteController.java
@@ -3,19 +3,19 @@ package com.poseidon.controller;
 import com.poseidon.dao.PacienteDao;
 import com.poseidon.model.Paciente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
 public class PacienteController {
     @Autowired
-    private PacienteDao pacienteDao;
+    private PacienteDao repositories;
 
     private static Logger logger = Logger.getLogger("PacienteController");
 
@@ -34,15 +34,21 @@ public class PacienteController {
 
     @RequestMapping(value = "/salvarPaciente", method = RequestMethod.POST)
     public ModelAndView salvarPaciente(@ModelAttribute Paciente paciente, ModelAndView modelAndView) {
-        pacienteDao.save(paciente);
+        repositories.save(paciente);
         logger.info("salvou paciente.");
         modelAndView.setViewName("home");
         return modelAndView;
     }
 
     @RequestMapping("/pesquisarPaciente")
-    public ModelAndView pesquisarPaciente(@ModelAttribute Paciente pacientes, ModelAndView modelAndView){
-        modelAndView.setViewName("pesquisaPaciente");
+    public ModelAndView pesquisarPaciente(ModelAndView modelAndView){
+        modelAndView.setViewName("pesquisarPaciente");
+        return modelAndView;
+    }
+
+    public ModelAndView pesquisaPacientes(ModelAndView modelAndView, @ModelAttribute Paciente paciente){
+        List<Paciente> pacientes = (List<Paciente>) repositories.findAll();
+        modelAndView.getModelMap().addAttribute("pacientes",pacientes);
         return modelAndView;
     }
 
