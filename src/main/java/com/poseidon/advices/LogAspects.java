464b747bda8955820cbb1dc46c2e34d52005eef5
Will <willahrons@hotmail.com>
2016-01-03 22:52:50 -0200

    adicionando title em algumas paginas

--- src/main/java/com/poseidon/advices/LogAspects.java
+++ src/main/java/com/poseidon/advices/LogAspects.java
@@ -12,33 +12,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspects {
 
+	
 
+	@Before("execution(* com.poseidon.*..*(..))")
+	public void logServiceAccess(JoinPoint joinPoint) {
+		Logger LOGGER = createLog(joinPoint);
+		AtributosClasse atributosClasse = criaAtributosClasse(joinPoint);
+		LOGGER.info("Accessing {} os argumentos passados foram: {}", joinPoint.getSignature().toShortString(),
+				atributosClasse.getArgumentos());
+	}
 
-    @Before("execution(* com.poseidon.*..*(..))")
-    public void logServiceAccess(JoinPoint joinPoint) {
-        Logger LOGGER = createLog(joinPoint);
-        AtributosClasse atributosClasse = criaAtributosClasse(joinPoint);
-        LOGGER.info("Accessing {} os argumentos passados foram: {}", joinPoint.getSignature().toShortString(),atributosClasse.getArgumentos());
-    }
-    
-    private AtributosClasse criaAtributosClasse(JoinPoint joinPoint){
-    	return AtributosClasse.builderAtributosClasse(joinPoint);
-    }
+	private AtributosClasse criaAtributosClasse(JoinPoint joinPoint) {
+		return AtributosClasse.builderAtributosClasse(joinPoint);
+	}
 
 	private Logger createLog(JoinPoint joinPoint) {
 		Logger LOGGER = LoggerFactory.getLogger(joinPoint.getSignature().getName());
 		return LOGGER;
 	}
-    
-   @AfterThrowing(pointcut = "execution(* com.poseidon.*..*(..))", throwing = "e")
-    public void afterThrowingAdvice(JoinPoint joinPoint, Throwable e) {
-      Logger LOGGER = createLog(joinPoint);
-      AtributosClasse atributosClasse = criaAtributosClasse(joinPoint);
-      
-      LOGGER.info("Foi lancada uma excecao no metodo:"
-          + atributosClasse.getMetodoNome() + " com os seguintes argumentos: "
-          + atributosClasse.getArgumentos() + "\nand  exception: "
-          + e.getMessage(), e);
-    }
+
+	@AfterThrowing(pointcut = "execution(* com.poseidon.*..*(..))", throwing = "e")
+	public void afterThrowingAdvice(JoinPoint joinPoint, Throwable e) {
+		Logger LOGGER = createLog(joinPoint);
+		AtributosClasse atributosClasse = criaAtributosClasse(joinPoint);
+
+		LOGGER.info("Foi lancada uma excecao no metodo:" + atributosClasse.getMetodoNome()
+				+ " com os seguintes argumentos: " + atributosClasse.getArgumentos() + "\nand  exception: "
+				+ e.getMessage(), e);
+	}
 
 }
--- src/main/java/com/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidon/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class HomeController   {
 
 	@RequestMapping("/home")
 	public ModelAndView home(ModelAndView modelAndView) {
