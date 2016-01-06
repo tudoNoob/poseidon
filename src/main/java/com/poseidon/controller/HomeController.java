055a7adb8ee4bce1d185341eabcdae18e44baaac
Will <willahrons@hotmail.com>
2016-01-06 21:59:54 -0200

    limpando imports home

--- src/main/java/com/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidon/controller/HomeController.java
@@ -1,11 +1,9 @@
 package com.poseidon.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.poseidon.annotation.ValidateArgs;
 import com.poseidon.annotation.ViewName;
 
 @Controller
