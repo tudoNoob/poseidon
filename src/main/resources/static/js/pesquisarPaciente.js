28f250b582607342c75a84556826a300bc11ad9d
Will <willahrons@hotmail.com>
2016-01-08 00:12:40 -0200

    Refatorando o js pesquisaPaciente

--- src/main/resources/static/js/pesquisarPaciente.js
+++ src/main/resources/static/js/pesquisarPaciente.js
@@ -25,33 +25,7 @@ $(document)
 						if(pesquisaDados.length<10){
 							iterador=pesquisaDados.length;
 						}
-						for (i = 0; i < iterador; i++) {
-							var rowNumber = "row" + i
-							var row = "<tr id=\""
-									+ rowNumber
-									+ "\"></td><td >"
-									+ pesquisaDados[i].id
-									+ "</td><td >"
-									+ pesquisaDados[i].nome
-									+ "</td><td >"
-									+ pesquisaDados[i].cpf
-									+ "</td><td >"
-									+ pesquisaDados[i].email
-									+ "</td><td >"
-									+ pesquisaDados[i].telefone
-									+ "</td><td >"
-									+ pesquisaDados[i].celular
-									+ "</td><td >"
-									+ pesquisaDados[i].endereco
-									+ "</td><td >"
-									+ pesquisaDados[i].cep
-									+ "</td><td >"
-									+ pesquisaDados[i].data_de_nascimentoString
-									+ "</td><td >"
-									+ pesquisaDados[i].data_da_ultima_consultaString
-									+ "</td></tr>";
-							$("#firstRow").after(row);
-						}
+						paginacaoCriaRows(0,iterador,pesquisaDados);
 						var maxlength= 5;
 						if(pesquisaDados.length<5){
 							maxlength=pesquisaDados.length;
@@ -80,43 +54,47 @@ $(document)
 											if (num > 1) {
 												comeca = iterador - 10;
 											}
-											var id=0;
-											for (i = comeca; i < iterador; i++) {
-											
-												
-												var rowNumber = "row" + id
-												var row = "<tr id=\""
-														+ rowNumber
-														+ "\"></td><td >"
-														+ pesquisaDados[i].id
-														+ "</td><td >"
-														+ pesquisaDados[i].nome
-														+ "</td><td >"
-														+ pesquisaDados[i].cpf
-														+ "</td><td >"
-														+ pesquisaDados[i].email
-														+ "</td><td >"
-														+ pesquisaDados[i].telefone
-														+ "</td><td >"
-														+ pesquisaDados[i].celular
-														+ "</td><td >"
-														+ pesquisaDados[i].endereco
-														+ "</td><td >"
-														+ pesquisaDados[i].cep
-														+ "</td><td >"
-														+ pesquisaDados[i].data_de_nascimentoString
-														+ "</td><td >"
-														+ pesquisaDados[i].data_da_ultima_consultaString
-														+ "</td></tr>";
-												$("#firstRow").after(row);
-												id=id+1;
-											}
+											paginacaoCriaRows(comeca,iterador,pesquisaDados);
 											$(this).bootpag({
 												total : pesquisaDados.length,
 												maxVisible : 5
 											});
 										});
 
+					}
+					
+					function paginacaoCriaRows(comeca,iterador,pesquisaDados){
+						var id=0;
+						for (i = comeca; i < iterador; i++) {
+						
+							
+							var rowNumber = "row" + id
+							var row = "<tr id=\""
+									+ rowNumber
+									+ "\"></td><td >"
+									+ pesquisaDados[i].id
+									+ "</td><td >"
+									+ pesquisaDados[i].nome
+									+ "</td><td >"
+									+ pesquisaDados[i].cpf
+									+ "</td><td >"
+									+ pesquisaDados[i].email
+									+ "</td><td >"
+									+ pesquisaDados[i].telefone
+									+ "</td><td >"
+									+ pesquisaDados[i].celular
+									+ "</td><td >"
+									+ pesquisaDados[i].endereco
+									+ "</td><td >"
+									+ pesquisaDados[i].cep
+									+ "</td><td >"
+									+ pesquisaDados[i].data_de_nascimentoString
+									+ "</td><td >"
+									+ pesquisaDados[i].data_da_ultima_consultaString
+									+ "</td></tr>";
+							$("#firstRow").after(row);
+							id=id+1;
+						}
 					}
 					
 				});
