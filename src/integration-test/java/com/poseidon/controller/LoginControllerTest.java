b3709db89af7648c946d734623b12bd0977739be
Will <willahrons@hotmail.com>
2016-01-10 20:57:37 -0200

    adicionando integration test

---
+++ src/integration-test/java/com/poseidon/controller/LoginControllerTest.java
@@ -0,0 +1,24 @@
+package com.poseidon.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBuildLogionPage() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuildLogionErrorPage() {
+		//fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/poseidon/controller/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package com.poseidon.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testHome() {
+		//fail("Not yet implemented");
+	}
+
+}
