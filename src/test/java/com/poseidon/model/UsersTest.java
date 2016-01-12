2dd89933993a49d4801981643a0772334a3c0b21
Pedro Silva <pedrro2@hotmail.com>
2016-01-12 16:23:12 -0200

    removendo linhas desnecessárias do teste

--- src/test/java/com/poseidon/model/UsersTest.java
+++ src/test/java/com/poseidon/model/UsersTest.java
@@ -1,14 +1,10 @@
 package com.poseidon.model;
 
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by psilva on 1/12/16.
- */
 public class UsersTest {
 
     UserView userView;
