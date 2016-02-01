1b66b0d532d9653e9bc7ca445fbe0605566cbc31
willmenn <willahrons@hotmail.com>
2016-02-01 06:09:57 -0200

    removendo teste duplicado

--- src/test/java/com/poseidon/utils/PoseidonUtilsTest.java
+++ src/test/java/com/poseidon/utils/PoseidonUtilsTest.java
@@ -14,16 +14,6 @@ import com.poseidon.model.ContaView;
 public class PoseidonUtilsTest {
 
     @Test
-    public void testConvertToDate() throws Exception {
-        java.util.Date date = new java.util.Date();
-        Date sqlDate = new Date(date.getTime());
-        date.getTime();
-        String dateString = PoseidonUtils.convertDateToString(date);
-
-        assertThat(PoseidonUtils.convertToDate(dateString)).isEqualToIgnoringHours(sqlDate);
-    }
-
-    @Test
     public void testConvertDateToString() throws Exception {
         java.util.Date date = new java.util.Date();
         SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
