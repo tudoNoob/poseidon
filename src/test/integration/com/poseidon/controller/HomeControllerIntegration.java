f87636b205bb6c36e73110f01e225b85d4df6929
Pedro Silva <pedrro2@hotmail.com>
2016-02-07 19:38:22 -0200

    @pedrro - retirando import n usado

--- src/test/integration/com/poseidon/controller/HomeControllerIntegration.java
+++ src/test/integration/com/poseidon/controller/HomeControllerIntegration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
