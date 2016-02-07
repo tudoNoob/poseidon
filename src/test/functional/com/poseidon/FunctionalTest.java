e1dca26d11c07580f9286119326911277fc3b3a7
JaderTS <jader-fernandes@live.com>
2016-02-07 21:51:40 -0200

    adicionando teste funcional de Admin como Login

--- src/test/functional/com/poseidon/FunctionalTest.java
+++ src/test/functional/com/poseidon/FunctionalTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -20,9 +21,15 @@ public class FunctionalTest extends PoseidonApplicationTests {
     @Test
     public void shouldReturnTitleOfPage() {
         driver.get("http://localhost:8081/");
-        String title = "Login";
+        String title = "Home";
+        String user = "admin";
+        String password = "admin";
+        driver.findElement(By.id("user.login")).sendKeys(user);
+        driver.findElement(By.id("password.login")).sendKeys(password);
+        driver.findElement(By.id("submit.login")).click();
         Assert.assertEquals(title,driver.getTitle());
     }
+
 
     @After
     public void tearDown() throws Exception {
