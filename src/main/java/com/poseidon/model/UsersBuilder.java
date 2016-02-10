33195d8212ae864e55cff90f747a1f73cbf6b169
William Ahrons <william.ahrons@gmail.com>
2016-02-10 00:28:15 -0200

    add contacontroller unit test

--- src/main/java/com/poseidon/model/UsersBuilder.java
+++ src/main/java/com/poseidon/model/UsersBuilder.java
@@ -1,18 +1,33 @@
 package com.poseidon.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by wahrons on 2/9/16.
  */
 public class UsersBuilder {
 
     private Users  myObject;
+    private List<Users> usersList;
 
     public UsersBuilder(){
         myObject= new Users();
+        usersList= new ArrayList<>();
     }
 
     public Users build(){
         return myObject;
+    }
+
+    public List<Users> buildAsList(){
+        return usersList;
+    }
+
+    public UsersBuilder add(){
+        usersList.add(myObject);
+        myObject= new Users();
+        return this;
     }
 
     public UsersBuilder withPassword(String password){
