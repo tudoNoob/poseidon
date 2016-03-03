5f59ffe8b03c84934d5928405c95fd6856ddcc96
William Ahrons <william.ahrons@gmail.com>
2016-03-03 18:34:53 -0300

    fixing error on url ../*

--- src/main/java/com/poseidon/controller/HomeController.java
+++ src/main/java/com/poseidon/controller/HomeController.java
@@ -6,17 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/user/")
 public class HomeController {
 
-	@RequestMapping("/homePage")
+	@RequestMapping("/user/homePage")
 	@ViewName(name = "home")
 	public ModelAndView home(ModelAndView modelAndView) {
 		return modelAndView;
 	}
 
 	@RequestMapping("/")
-	@ViewName(name = "redirect:homePage")
+	@ViewName(name = "redirect:/user/homePage")
 	public ModelAndView defaultUrl(ModelAndView modelAndView) {
 		return modelAndView;
 	}
---
+++ src/test/java/com/poseidon/utils/RedirectAttributesMock.java
@@ -0,0 +1,7 @@
+package com.poseidon.utils;
+
+/**
+ * Created by wahrons on 3/3/16.
+ */
+public class RedirectAttributesMock {
+}
