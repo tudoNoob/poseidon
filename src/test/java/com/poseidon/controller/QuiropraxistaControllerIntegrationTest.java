a76dbdc594c27d0fc10dac8fc2912f9f97677db2
William Ahrons <william.ahrons@gmail.com>
2016-04-26 18:22:07 -0300

    Refatorando o nome do QuiropraxistaControllerIntegrationTest

--- src/test/java/com/poseidon/controller/QuiropraxistaControllerIntegrationTest.java
+++ src/test/java/com/poseidon/controller/QuiropraxistaControllerIntegrationTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 
-public class QuiropraxistaControlerIntegrationTest extends PoseidonApplicationTests {
+public class QuiropraxistaControllerIntegrationTest extends PoseidonApplicationTests {
 
     private MockMvc mockMvc;
 
