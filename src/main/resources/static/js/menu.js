eb38f9e5853c69a0c6f0d22d854622deec4c31e0
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-05-04 21:25:40 -0300

    Padronização de importações e parte do header modificados

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,27 +1,21 @@
 /**
  * Este arquivo é só para scprits relacionados ao header das páginas.
  */
-
-$(document).ready(function(){
-	
-	
-$( ".menu" ).each(function( index ) {
-			$(this).removeClass('active');
-			var link=$(this).children();
-			  var url= $(link).attr('url');
-			  var pathname=window.location.pathname;
-			  if(url===pathname){
-				  $(this).addClass('active');
-				  link.removeClass('colorBranco');
-				  link.addClass("focusedMenu");
-			  }
-			});
-		
-		$( ".menu a" ).hover(function() {
-			  $(this).addClass("focusedMenu");
-			},function(){
-				$(this).removeClass("focusedMenu");
-			});
-		
-		
+$(function () {
+    $(".colorBranco").click(function(){
+        $(this).addClass('colorBrancoClicked');
+    });
+    $(".menu").each(function (index) {
+        $(this).find('li').find('a').removeClass('active');
+        var url = $(this).find('li').attr('name');
+        var pathname = window.location.pathname;
+        console.log(link);
+        console.log(url);
+        console.log(pathname);
+        console.log($(this).find('li'));
+        if (url === pathname) {
+            $(this).find('li').find('a').addClass('colorBrancoActive');
+        }
+    });
+    console.log($(".menu"));
 });
