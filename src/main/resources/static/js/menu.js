a5daa17ac3264d221f09ce47ab4cb0494773beb0
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-05-04 22:34:36 -0300

    Header quase totalmente ajustado

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -2,20 +2,12 @@
  * Este arquivo é só para scprits relacionados ao header das páginas.
  */
 $(function () {
-    $(".colorBranco").click(function(){
-        $(this).addClass('colorBrancoClicked');
-    });
-    $(".menu").each(function (index) {
-        $(this).find('li').find('a').removeClass('active');
-        var url = $(this).find('li').attr('name');
+    $(".menu").find('li').each(function (index) {
+        $(this).find('a').removeClass('colorBrancoActive');
+        var url = $(this).attr('name');
         var pathname = window.location.pathname;
-        console.log(link);
-        console.log(url);
-        console.log(pathname);
-        console.log($(this).find('li'));
         if (url === pathname) {
-            $(this).find('li').find('a').addClass('colorBrancoActive');
+            $(this).find('a').addClass('colorBrancoActive');
         }
     });
-    console.log($(".menu"));
 });
